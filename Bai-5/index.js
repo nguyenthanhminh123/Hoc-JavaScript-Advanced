749553ccd3d0da1258e1e8af579a73504e3c210d
Nguyen Thanh Minh <thanhminh339968az@gmail.com>
2023-04-10 21:56:08 +0700

    Bài 5 - Arrow function expression (Part 1)

---
+++ Bai-5/index.js
@@ -0,0 +1,28 @@
+// arrow function expression
+// fat arrow
+// functinon sum (a,b){
+//  return a + b;
+//}
+// sum(1, 2);
+
+// var sum2 = function(a, b) {
+// return a + b;
+//}
+// sum2(3, 2);
+// var sum = (a, b) => {
+// return a + b;
+//};
+
+// var sum = (a, b) => a + b;
+// sum(4, 8);
+
+// var square = (x) => x*x;
+// var square = x => x*x;
+// square(5);
+
+// var doSomething = () => Date.now();
+// doSomething();
+var arr = [1,2,3];
+arr.map(function(x){
+  return x*x;
+})
