9b750faefcfec43934f9566ee7ef57c6a0d4bd87
Nguyen Thanh Minh <thanhminh339968az@gmail.com>
2023-04-11 16:27:20 +0700

    Bai-13 apply

---
+++ Bai-13/index.js
@@ -0,0 +1,15 @@
+// apply
+// call: func.call(this. param1, param2, ...);
+// apply: func.apply(this, [param1, param2, ...])
+function sum (){
+  const numbers = Array.from(arguments);
+  return numbers.reduce((sum, num) => sum + num, 0);
+}
+
+function average(){
+  //sum
+  const x = sum.apply(null, arguments);
+  // sum  /arguments.length
+  return x/arguments.length;
+}
+average(1, 2, 3, 6);
