3097210a7a27c365f1dee4efeeaf3079efb863f4
Nguyen Thanh Minh <thanhminh339968az@gmail.com>
2023-04-11 16:05:08 +0700

    Bai-7 Teamlate string

---
+++ Bai-7/index.js
@@ -0,0 +1,11 @@
+// Teamlate string 
+function greeting(name){
+  return 'hi, ' + name + '!';
+}
+
+function greeting2(name){
+  return `hi, ${name}!`;
+}
+
+var result = greeting2('Minh');
+console.log(result);
